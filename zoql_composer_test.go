f450c7c19a8877d448650fbae422576a0502a115
Hector Yeomans <yeomans.leonel@gmail.com>
2019-08-24 10:02:59 -0700

    Updating Zoql composer.

--- zoql_composer_test.go
+++ zoql_composer_test.go
@@ -1,7 +1,6 @@
 package zuora_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/hyeomans/zuora"
@@ -11,9 +10,10 @@ func TestComposerWithNoFilters(t *testing.T) {
 	t.Parallel()
 	expectedResult := `{ "queryString" : "select ID, Name from Product" }`
 
-	fields := []string{"ID", "Name"}
-	zoqlComposer := zuora.NewZoqlComposer("Product", fields)
-	result := fmt.Sprint(zoqlComposer)
+	result := zuora.NewZoqlComposer().
+		Fields("ID", "Name").
+		From("Product").
+		Build()
 
 	if result != expectedResult {
 		t.Fatalf("Zoql Composer failed to build a simple query. Expected: `%v` but got `%v`", expectedResult, result)
@@ -24,12 +24,7 @@ func TestComposerWithSingleFilter(t *testing.T) {
 	t.Parallel()
 	expectedResult := `{ "queryString" : "select ID, Name from Product where Name = 'productName'" }`
 
-	fields := []string{"ID", "Name"}
-	queryFilter := zuora.QueryFilter{Key: "Name", Value: "productName"}
-	singleFilter := zuora.QueryWithFilter(queryFilter)
-	zoqlComposer := zuora.NewZoqlComposer("Product", fields)
-	singleFilter(zoqlComposer)
-	result := fmt.Sprint(zoqlComposer)
+	result := zuora.NewZoqlComposer().Fields("ID", "Name").From("Product").Where("Name", "productName").Build()
 
 	if result != expectedResult {
 		t.Fatalf("Zoql Composer failed to build a simple query. Expected: `%v` but got `%v`", expectedResult, result)
@@ -40,16 +35,7 @@ func TestComposerWithOrFilter(t *testing.T) {
 	t.Parallel()
 	expectedResult := `{ "queryString" : "select ID, Name from Product where Name = 'productName' or Name = 'productName'" }`
 
-	fields := []string{"ID", "Name"}
-	singleFilter := zuora.QueryFilter{Key: "Name", Value: "productName"}
-	queryFilter := []zuora.QueryFilter{singleFilter}
-	addSingleFilter := zuora.QueryWithFilter(singleFilter)
-	addOrFilter := zuora.QueryWithOrFilter(queryFilter)
-
-	zoqlComposer := zuora.NewZoqlComposer("Product", fields)
-	addSingleFilter(zoqlComposer)
-	addOrFilter(zoqlComposer)
-	result := fmt.Sprint(zoqlComposer)
+	result := zuora.NewZoqlComposer().Fields("ID", "Name").From("Product").Where("Name", "productName").Or("Name", "productName").Build()
 
 	if result != expectedResult {
 		t.Fatalf("Zoql Composer failed to build a simple query. Expected: `%v` but got `%v`", expectedResult, result)
@@ -60,38 +46,7 @@ func TestComposerWithAndFilter(t *testing.T) {
 	t.Parallel()
 	expectedResult := `{ "queryString" : "select ID, Name from Product where Name = 'productName' and Name = 'productName'" }`
 
-	fields := []string{"ID", "Name"}
-	singleFilter := zuora.QueryFilter{Key: "Name", Value: "productName"}
-	queryFilter := []zuora.QueryFilter{singleFilter}
-	addSingleFilter := zuora.QueryWithFilter(singleFilter)
-	addAndFilter := zuora.QueryWithAndFilter(queryFilter)
-
-	zoqlComposer := zuora.NewZoqlComposer("Product", fields)
-	addSingleFilter(zoqlComposer)
-	addAndFilter(zoqlComposer)
-	result := fmt.Sprint(zoqlComposer)
-
-	if result != expectedResult {
-		t.Fatalf("Zoql Composer failed to build a simple query. Expected: `%v` but got `%v`", expectedResult, result)
-	}
-}
-
-func TestComposerWithAllFilters(t *testing.T) {
-	t.Parallel()
-	expectedResult := `{ "queryString" : "select ID, Name from Product where Name = 'productName' and Name = 'productName' and Name = 'productName' and Name = 'productName' and Name = 'productName' or Name = 'productName' or Name = 'productName' or Name = 'productName' or Name = 'productName'" }`
-
-	fields := []string{"ID", "Name"}
-	singleFilter := zuora.QueryFilter{Key: "Name", Value: "productName"}
-	filters := []zuora.QueryFilter{singleFilter, singleFilter, singleFilter, singleFilter}
-	addSingleFilter := zuora.QueryWithFilter(singleFilter)
-	addAndFilter := zuora.QueryWithAndFilter(filters)
-	addOrFilter := zuora.QueryWithOrFilter(filters)
-	zoqlComposer := zuora.NewZoqlComposer("Product", fields)
-	addSingleFilter(zoqlComposer)
-	addAndFilter(zoqlComposer)
-	addOrFilter(zoqlComposer)
-
-	result := fmt.Sprint(zoqlComposer)
+	result := zuora.NewZoqlComposer().Fields("ID", "Name").From("Product").Where("Name", "productName").And("Name", "productName").Build()
 
 	if result != expectedResult {
 		t.Fatalf("Zoql Composer failed to build a simple query. Expected: `%v` but got `%v`", expectedResult, result)
