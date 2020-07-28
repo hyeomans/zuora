f1d097a4128b3a11a9e9c35ab54987dfe17b428a
Hector Yeomans <yeomans.leonel@gmail.com>
2020-07-28 15:58:47 -0700

    Added create account endpoint (#19)

--- accounts.go
+++ accounts.go
@@ -249,3 +249,80 @@ func (t *accountsService) Update(ctx context.Context, objectID string, account i
 
 	return jsonResponse, nil
 }
+
+// Update - Updates a customer account by specifying the account-key.
+func (t *accountsService) Create(ctx context.Context, account interface{}) (Response, error) {
+	authHeader, err := t.authHeaderProvider.AuthHeaders(ctx)
+
+	if err != nil {
+		return Response{}, responseError{isTemporary: false, message: fmt.Sprintf("error while trying to set auth headers: %v", err)}
+	}
+
+	url := fmt.Sprintf("%v/v1/accounts", t.baseURL)
+
+	j, err := json.Marshal(account)
+
+	if err != nil {
+		return Response{}, responseError{isTemporary: false, message: fmt.Sprintf("error while trying to convert empty interface: %v", err)}
+	}
+
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(j))
+
+	if err != nil {
+		return Response{}, responseError{isTemporary: false, message: fmt.Sprintf("error while trying to create an HTTP request: %v", err)}
+	}
+
+	req.Header.Add("Authorization", authHeader)
+	req.Header.Add("Content-Type", "application/json")
+
+	if ctx.Value(ContextKeyZuoraEntityIds) != nil {
+		req.Header.Add("Zuora-Entity-Ids", ctx.Value(ContextKeyZuoraEntityIds).(string))
+	}
+
+	if ctx.Value(ContextKeyZuoraTrackID) != nil {
+		req.Header.Add("Zuora-Track-Id", ctx.Value(ContextKeyZuoraTrackID).(string))
+	}
+
+	res, err := t.http.Do(req.WithContext(ctx))
+
+	if err != nil {
+		return Response{}, responseError{isTemporary: false, message: fmt.Sprintf("error while trying to make request: %v", err)}
+	}
+
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+
+	if res.StatusCode < 200 || res.StatusCode > 299 {
+		var isTemporary bool
+		if http.StatusRequestTimeout == res.StatusCode ||
+			http.StatusTooManyRequests == res.StatusCode ||
+			http.StatusInternalServerError == res.StatusCode ||
+			http.StatusServiceUnavailable == res.StatusCode {
+			isTemporary = true
+		}
+
+		if err != nil {
+			return Response{}, responseError{isTemporary: isTemporary, message: fmt.Sprintf("error while trying to read body response into memory. Response Code: %v - Error: %v", res.StatusCode, err)}
+		}
+
+		return Response{}, responseError{isTemporary: isTemporary, message: fmt.Sprintf("got an invalid http status. Response Code: %v - Body: %v", res.StatusCode, string(body))}
+	}
+
+	jsonResponse := Response{}
+
+	if err := json.Unmarshal(body, &jsonResponse); err != nil {
+		return Response{}, responseError{isTemporary: false, message: fmt.Sprintf("error while Unmarshal json response. Error: %v. JSON: %v", err, string(body))}
+	}
+
+	if !jsonResponse.Success {
+		errorResponse := errorResponse{}
+
+		if err := json.Unmarshal(body, &errorResponse); err != nil {
+			return Response{}, responseError{isTemporary: false, message: fmt.Sprintf("error while Unmarshal json error response. Error: %v. Raw JSON: %v", err, string(body))}
+		}
+
+		return Response{}, errorResponse
+	}
+
+	return jsonResponse, nil
+}
